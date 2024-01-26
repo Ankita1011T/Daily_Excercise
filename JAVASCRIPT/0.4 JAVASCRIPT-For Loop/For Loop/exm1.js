e297acee46efd878491765fd4c795f7ae8bfdc03
Ankita Tribhuvan <ankitatribhuvan6@gmail.com>
2024-01-26 22:24:09 +0530

    For Loop

---
+++ JAVASCRIPT/0.4 JAVASCRIPT-For Loop/For Loop/exm1.js
@@ -0,0 +1,4 @@
+let a = 1;
+for( let i = 0; i < 100; i++ ){
+     console.log( a + i );
+}
---
+++ JAVASCRIPT/0.4 JAVASCRIPT-For Loop/For Loop/exm3.js
@@ -0,0 +1,5 @@
+//print 1 to 5 
+for(let count  = 1; count <= 200; count ++){
+    console.log("I Am Leanring JAVASCRIPT");
+}
+console.log("loop has ended");
