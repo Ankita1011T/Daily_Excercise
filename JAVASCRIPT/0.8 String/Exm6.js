46aebee79986f5b7be375a58159f06834ad81543
Ankita Tribhuvan <ankitatribhuvan6@gmail.com>
2024-02-01 22:50:26 +0530

    HTML - Input type

---
+++ JAVASCRIPT/0.8 String/Exm6.js
@@ -0,0 +1,17 @@
+let a = "JORDEN";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a.length);
+
+let b = "God Shiva";
+console.log(b.toUpperCase()); //   to upper and to lower function
+console.log(b.slice(0,8)); //  Slice function
+console.log(b.concat("BHOLENATH","MAHADEV")); // conact function
+console.log(b.length); // string length Properties
+console.log(b.replace('Shiva','Infi'));  // replace function
+console.log(b.includes('1'));
