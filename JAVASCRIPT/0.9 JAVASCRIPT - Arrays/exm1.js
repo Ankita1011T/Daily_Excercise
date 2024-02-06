bf284fb7bbea6d2ca5fc63ae1a5ed2ee1ad7db6d
Ankita Tribhuvan <ankitatribhuvan6@gmail.com>
2024-02-06 22:27:09 +0530

    Types of Arrays in JAVASCRIPT

---
+++ JAVASCRIPT/0.9 JAVASCRIPT - Arrays/exm1.js
@@ -0,0 +1,6 @@
+let marks = [99, 85, 77, 66, 55];
+console.log(marks);
+
+let students = ["venkatesh", "aditya", "nimit", "hitesh"];
+console.log(students);
+
---
+++ JAVASCRIPT/0.9 JAVASCRIPT - Arrays/exm2.js
@@ -0,0 +1,4 @@
+//array indices
+let number = [ 86, 66, 96, 88, 75, 11];
+number[5] = 10;
+console.log(number);
