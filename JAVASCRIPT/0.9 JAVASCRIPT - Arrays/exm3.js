9455840b7d6f0a04d43e9c07624d43e88dba2268
Ankita Tribhuvan <ankitatribhuvan6@gmail.com>
2024-02-07 22:22:23 +0530

    ARRAY in Javascript

---
+++ JAVASCRIPT/0.9 JAVASCRIPT - Arrays/exm3.js
@@ -0,0 +1,9 @@
+//Looping over an array
+
+//print all elements of an array:
+
+let heroes = ["ironman", "spiderman", "batman", "thor", "hulk"];
+
+for( let i = 0; i < heroes.length; i++) {
+    console.log(heroes[i]);
+}  
---
+++ JAVASCRIPT/0.9 JAVASCRIPT - Arrays/exm4.js
@@ -0,0 +1,7 @@
+// for of loops in arrays.
+
+let superhero = ["iron", "spiderman", "batman", "hulk", "thor"];
+
+for ( let hero of superhero){
+    console.log(hero.toLocaleUpperCase());
+}
