9e47c1a22f13dafacb00f49b5ad382f881aa2d2c
Ankita Tribhuvan <ankitatribhuvan6@gmail.com>
2024-02-15 21:34:50 +0530

    Operators in js

---
+++ JAVASCRIPT/0.3 JAVASCRIPT-Operators & conditional statments/Examples/exm1.js
@@ -0,0 +1,18 @@
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+console.log(z);
+
+let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
+console.log(text3);
+
+
+// Assign the value 5 to x
+let number = 5;
+// Assign the value 2 to y
+let value = 2;
+// Assign the value x + y to z:
+let result = x + y;
+console.log(result);
