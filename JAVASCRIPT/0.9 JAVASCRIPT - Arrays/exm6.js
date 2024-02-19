56286c51df466ae084a7f644148648acdbae9786
Ankita Tribhuvan <ankitatribhuvan6@gmail.com>
2024-02-19 22:42:18 +0530

    Js

---
+++ JAVASCRIPT/0.9 JAVASCRIPT - Arrays/exm6.js
@@ -0,0 +1,15 @@
+let items = ['milk','bread','butter','coffe']
+console.log(items[3])
+items[3] = 'tea'
+console.log(items)
+items.push('strawbeeries')
+
+let username = ['ankitatribhuvan4@gmail.com','evenky189@gmail.com','sureshtribhuvanST@gmail.com']
+console.log(username)
+console.log(username[1])
+username[0] = 'ankitatribhuvan6@gmail.com'
+console.log(username)
+username.push('adityathakur11@gmail.com')
+
+
+
