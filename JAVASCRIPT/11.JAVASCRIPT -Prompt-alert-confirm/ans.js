cbcfb2841a4f6ff7aef051be4f5cf8a6ab2dec49
Ankita Tribhuvan <ankitatribhuvan6@gmail.com>
2024-03-05 19:00:18 +0530

    Interaction - alert,prompt,confirm in js

---
+++ JAVASCRIPT/11.JAVASCRIPT -Prompt-alert-confirm/ans.js
@@ -0,0 +1,16 @@
+// Alert in  in-browser javascript - does not return anything.
+alert("This is a Message");
+
+let question = prompt("Are you a beginner in JAVASCRIPT?");
+console.log(question);
+
+let deletePost = confirm("Do you really want to delete this post?");
+
+if(deletePost){
+    console.log("your post has been deleted successfully");
+}
+else{
+    console.log("your post has not been deleted");
+}
+
+
