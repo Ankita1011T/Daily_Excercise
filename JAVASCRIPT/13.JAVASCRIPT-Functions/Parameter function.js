8f1d444fc5530dbb00577077f7022852d78236c9
Ankita1011T <ankitatribhuvan6@gmail.com>
2024-03-12 22:40:39 +0530

    Functions in JAVASCRIPT

---
+++ JAVASCRIPT/13.JAVASCRIPT-Functions/Parameter function.js
@@ -0,0 +1,8 @@
+
+function myFunction(msg, n){
+    // parameter --> input
+    console.log(msg * n );
+}
+
+myFunction("I love JAVASCRIPT", 100); //arguments
+
---
+++ JAVASCRIPT/13.JAVASCRIPT-Functions/funtions.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    console.log("Today i am learing about functions in javascript.");
+    console.log("We are learning JS.");
+}
+
+myFunction();
+myFunction();
+myFunction();
+myFunction();
