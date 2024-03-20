041a606154553ee2ffab9dcc8bf4b105a025db10
Ankita1011T <ankitatribhuvan6@gmail.com>
2024-03-20 22:29:47 +0530

    var let const in javascript

---
+++ JAVASCRIPT/14.JAVASCRIPT - Var, let, const/script.js
@@ -0,0 +1,17 @@
+// var name; // variable declaration
+// var name = "tvisha"; // variable ko value assign krna
+// console.log(name);
+
+
+//const age = 20;
+//console.log(age);
+
+// var num = 50;
+// var num; // re-declare 
+// var num = 60; // re-assign
+// var num = 355656.65
+// console.log(num)
+
+let num = 50;
+num = 60;
+console.log(num);
