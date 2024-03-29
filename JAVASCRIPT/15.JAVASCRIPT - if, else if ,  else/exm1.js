e76a3d1aade5ea431ab50cf3e79b24c885e97f68
Ankita1011T <ankitatribhuvan6@gmail.com>
2024-03-29 21:42:48 +0530

    if else statment in JAVASCRIPT

---
+++ JAVASCRIPT/15.JAVASCRIPT - if, else if ,  else/exm1.js
@@ -0,0 +1,8 @@
+let age = 21;
+
+if(age>=18){
+    console.log("You Can Drive");
+}
+else {
+    console.log("You Can not drive");
+}
---
+++ JAVASCRIPT/15.JAVASCRIPT - if, else if ,  else/exm2.js
@@ -0,0 +1,13 @@
+let age = prompt("Enter your age: ")
+
+if (age <= 10){
+    console.log("They all are kids")
+}
+else if(age >= 11 && age<= 17 ){
+    console.log("They all are teenage")
+}
+else if (age >= 18 && age <= 30){
+    console.log("They all are adults ")
+}else{
+    console.log("Enjoy your life")
+}
