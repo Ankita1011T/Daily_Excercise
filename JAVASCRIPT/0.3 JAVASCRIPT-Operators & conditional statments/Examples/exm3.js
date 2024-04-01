02c5d2ded3a9efaaa0a8894f045760d0ff1ff53e
Ankita1011T <ankitatribhuvan6@gmail.com>
2024-04-01 22:43:19 +0530

    Operators and conditional statments in JS

---
+++ JAVASCRIPT/0.3 JAVASCRIPT-Operators & conditional statments/Examples/exm3.js
@@ -0,0 +1,16 @@
+// Arthmethic Operators :
+
+let a = 5;
+let b = 2;
+let c = a + b;
+
+
+console.log("a =",a ,"b =", b);
+console.log("a + b :", a + b );
+console.log("a - b :", a - b );
+console.log("a * b :", a * b );
+console.log("a / b :", a / b );
+console.log("a % b :", a % b );
+console.log("a ** b :", a ** b);
+console.log( c );
+
