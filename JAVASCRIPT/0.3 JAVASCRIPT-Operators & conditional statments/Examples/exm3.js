a6ce220fabc0a6939efd2fedf37e69a27123e6fc
Ankita1011T <ankitatribhuvan6@gmail.com>
2024-04-03 21:28:38 +0530

    Datatypes and Operators in JAVASCRIPTS

--- JAVASCRIPT/0.3 JAVASCRIPT-Operators & conditional statments/Examples/exm3.js
+++ JAVASCRIPT/0.3 JAVASCRIPT-Operators & conditional statments/Examples/exm3.js
@@ -3,6 +3,8 @@
 let a = 5;
 let b = 2;
 let c = a + b;
+let name = "operators and conditional statments"
+let pass = true
 
 
 console.log("a =",a ,"b =", b);
@@ -13,4 +15,15 @@ console.log("a / b :", a / b );
 console.log("a % b :", a % b );
 console.log("a ** b :", a ** b);
 console.log( c );
+console.log(typeof a);
+console.log(typeof name);
+console.log(typeof pass);
+
+const Ytlecture = {
+    video        : 'https://akjhdakjdhakjhda',
+    thumbnail    :  'https://adjhajkdhkajhda',
+    descriptions : 'adjashdkjahkj dmdskskfndkdfanaannanaamsndnskd',
+    tags         : 'lectures 25, lectures 27',
+};
+console.log(typeof Ytlecture);
 
---
+++ JAVASCRIPT/0.3 JAVASCRIPT-Operators & conditional statments/Examples/exm4.js
@@ -0,0 +1,48 @@
+// arithmetic operator's
+
+let sum = 4 + 5;
+console.log(sum);
+
+const num1 = 5;
+const num2 = 20;
+
+let multi = (num1 / num2);
+console.log(multi);
+
+let pow = 5 ** 2;
+console.log(pow);
+
+// Assignment operators 
+
+let val = 5;
+val += 10;
+console.log(val);
+
+let val1 = 25;
+val1 -= 20;
+console.log(val1);
+
+let val2 = 35;
+val2 *= 2;
+console.log(val2);
+
+let val3 = 50;
+val3 /= 2;
+console.log(val3);
+
+let val4 = 75;
+val4 %= 2;
+console.log(val4);
+
+// unary operators:
+
+let count = 10;
+count ++ ;
+console.log(count);
+
+let count1 = 10;
+-- count; 
+console.log(count);
+
+let sum1 = 1 + '1' + 'Code' ;
+console.log(sum1);
