8ad96f28705fc75948924e4269639f93dcc308e3
Ankita1011T <ankitatribhuvan6@gmail.com>
2024-04-10 21:28:40 +0530

    Controlflow & Loops

---
+++ JAVASCRIPT/18..JAVASCRIPT - Control flow & Loops/exm1.js
@@ -0,0 +1,17 @@
+// While Loop statements "
+
+let num = 0;
+
+while(num <= 55){
+    console.log(num);// infinte loop
+    num++;
+}
+
+// do-while loop statement :
+
+let num1 = 0;
+
+do{
+    console.log(num1);// infinte loop
+    num1++;
+}while(num1 <= 8)
---
+++ JAVASCRIPT/18..JAVASCRIPT - Control flow & Loops/script.js
@@ -0,0 +1,4 @@
+// TERNARY Conditional Statement :
+
+let age = 31;
+console.log(( age >= 18 ) ? "You can vote" : "You can not vote");
